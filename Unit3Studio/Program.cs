2527b25574a2a5db7e55cf59a419a0b6be25bc55
dennis-phillips <dennis_phillips@outlook.com>
2020-06-03 08:42:30 -0400

    Unit 3 Studio Complete

--- Unit3Studio/Program.cs
+++ Unit3Studio/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Unit3Studio
 {
     class Program
     {
+
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string inputString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus";
+            char[] charArray = inputString.ToCharArray();
+            //Console.WriteLine(stringArray.Length);
+            Dictionary<char, int> CharacterDictionary = new Dictionary<char, int>();
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                
+                if(!CharacterDictionary.ContainsKey(charArray[i]))
+                {
+                    CharacterDictionary.Add(charArray[i], 1);
+                }
+                else
+                {
+                    CharacterDictionary[charArray[i]] += 1;
+                }
+            }
+            foreach (KeyValuePair<char, int> key in CharacterDictionary)
+            {
+                Console.WriteLine(key.Key + ":" + key.Value);
+            }
+            //Console.WriteLine("Hello World!");
         }
     }
 }
